92687e13321b5092a6812f03918f9031f262cda8
affanengi <mohammedaffanrazvi604@gmail.com>
2025-06-11 11:37:25 +0530

    3rd commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Erum Khan School Management Dashboard",
+  title: "Affan Razvi School Management Dashboard",
   description: "Next.js School Management System",
 };
 
